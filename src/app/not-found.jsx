58478cd174ffa6a665ee613bf8e1f40c24145f1a
Faraz1995 <53772471+Faraz1995@users.noreply.github.com>
2024-04-09 09:37:46 +0330

    add not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+function NotFoundPage() {
+  return (
+    <div>
+    <h2>not found</h2>
+    <Link href={''}> return to Homepage</Link>
+    </div>
+  )
+}
+
+export default NotFoundPage
